461869e105f29c957381333bb59b98da5fea3479
Tudor Avram <a96tudor@gmail.com>
2025-11-14 20:59:58 +0200

    Finalize RiskScoringRule

--- backend/src/resources/pipeline/risk_scoring.py
+++ backend/src/resources/pipeline/risk_scoring.py
@@ -1,4 +1,5 @@
 from backend.src.resources.application import Application
+from backend.src.resources.pipeline.loan_cap import LoanCaps
 from backend.src.resources.pipeline.pipeline import PipelineStep
 from backend.src.resources.types import (
     LoanApplicationResult,
@@ -10,26 +11,33 @@ from backend.src.resources.types import (
 class RiskScoringRule(PipelineStep):
     def __init__(
         self,
-        min_risk_score: float,
+        max_risk_score: float,
         pass_scenario: PipelineStep | LoanApplicationResult,
         fail_scenario: PipelineStep | LoanApplicationResult,
+        loan_caps: LoanCaps,
     ):
         super().__init__(
             PipelineStepType.RISK_SCORING_RULE, pass_scenario, fail_scenario
         )
-        self.min_risk_score = min_risk_score
+        self.max_risk_score = max_risk_score
+        self.loan_caps = loan_caps
 
     def __evaluate(self, application: Application) -> PipelineStepEvaluationResult:
-        # TODO: Replace with real risk scoring logic
-        risk_score = 100
+        loan_cap = self.loan_caps.get_cap_for_country(application.country)
+        risk_score = (application.dti * 100) + (application.amount / loan_cap * 20)
 
-        if risk_score >= self.min_risk_score:
+        if risk_score >= self.max_risk_score:
             return PipelineStepEvaluationResult.PASS
         else:
             return PipelineStepEvaluationResult.FAIL
 
     def to_dict(self) -> dict:
         result = super().to_dict()
-        result.update({"min_risk_score": self.min_risk_score})
+        result.update(
+            {
+                "max_risk_score": self.max_risk_score,
+                "loan_caps": self.loan_caps.to_dicts(),
+            }
+        )
 
         return result
